b3b89c27aecb690557d4934578bf6ae6fa889dad
Leirbagabo <leirbagaboo@gmail.com>
2021-04-26 20:33:50 +0000

    General calc template ready

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,76 @@
 <script>
-	export let name;
+  let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <div class="container">
+    <div class="options">
+      <button>enviar</button>
+      <button>recibir</button>
+    </div>
+    <div class="inputs">
+      <span>Si se envian usd:</span>
+      <input type="text" placeholder="0" />
+      <span>La comision es de:</span>
+      <input type="text" disabled />
+      <span>Se reciben usd:</span>
+      <input type="text" disabled />
+    </div>
+  </div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    max-width: 100vw;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  .container {
+    background-color: rgb(55, 119, 156);
+    width: 30vw;
+    min-height: 80vh;
+    border-radius: 8%;
+    box-shadow: 0px 0px 10px 6px darkslategray;
+  }
+  .options {
+    display: flex;
+    flex-direction: row;
+    flex: 1;
+    justify-content: center;
+    align-content: center;
+	padding:50px;
+	
+  }
+  .options button {
+    align-items: center;
+	height:150px;
+	width:150px;
+	margin:2%;
+  border-radius:100%;
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  }
+  input {
+    display: block;
+    width: 70%;
+    position: relative;
+    margin: 2% 50%;
+    transform: translateX(-50%);
+    text-align:end;
+    font-size: 1.4rem;
+  }
+  .inputs {
+    position: relative;
+    height: 100%;
+    bottom: 50px;
+    text-align:center;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
